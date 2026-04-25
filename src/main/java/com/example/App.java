ecf59239f0ecd7c47b716d671ab2c83f8b04ad79
kamalkarthikjadi-oss <kamalkarthikjadi@gmail.com>
2026-04-25 11:09:18 +0530

    failure test case

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,14 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello CI Pipeline!");
+        System.out.println("Sum: " + add(5, 3));
     }
 }
